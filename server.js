adf2941256074a21eb3ac793f237e282729ee46d
JulienA <ju.aubert87@gmail.com>
2014-09-01 18:10:00 +0200

    start.sh

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World tititit\n');
 }).listen(1337);
 console.log('Server running');
