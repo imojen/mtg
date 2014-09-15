413e237b271048d33fa490093ffe185bb7587ead
JulienA <ju.aubert87@gmail.com>
2014-09-15 20:43:14 +0200

    start script

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+#!/usr/local/bin/node
 cpt=`pgrep node | wc -l`
 if [ "$cpt" -lt 1 ]; then
-    nohup node /home/pi/gitRepo/mtg/server.js &
+    nohup /usr/local/bin/node /home/pi/gitRepo/mtg/server.js &
+    exit 0
 else
     exit 0
 fi
+exit 0
