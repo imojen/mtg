5ce713601bed4cab82a8d8f77cd698b5773f853a
imojen <souhami.nicolas@gmail.com>
2014-09-16 11:32:47 +0200

    start.sh modif temporaire

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #!/usr/local/bin/node
-cpt=`pgrep node | wc -l`
-if [ "$cpt" -lt 1 ]; then
+
+#cpt=`pgrep node | wc -l`
+#if [ "$cpt" -lt 1 ]; then
     nohup /usr/local/bin/node /home/pi/gitRepo/mtg/server.js &
     exit 0
-else
-    exit 0
-fi
-exit 0
+#else
+#    exit 0
+#fi
+#exit 0
