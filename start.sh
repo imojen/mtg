92a28da8b1b513f6beca057ce4568f60015241ce
imojen <souhami.nicolas@gmail.com>
2014-09-16 12:09:47 +0200

    remise a la normale du start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 #!/usr/local/bin/node
-
-nohup /usr/local/bin/node /home/pi/gitRepo/mtg/server.js &
-exit 0
-
-
-
-
 cpt=`pgrep node | wc -l`
 if [ "$cpt" -lt 1 ]; then
     nohup /usr/local/bin/node /home/pi/gitRepo/mtg/server.js &
