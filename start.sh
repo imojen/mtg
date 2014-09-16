9bbe7d03c5a4e1f49b068b6b94eef13422704a0d
imojen <souhami.nicolas@gmail.com>
2014-09-16 11:54:57 +0200

    start.sh petite modif

--- start.sh
+++ start.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #!/usr/local/bin/node
 
-#cpt=`pgrep node | wc -l`
-#if [ "$cpt" -lt 1 ]; then
+nohup /usr/local/bin/node /home/pi/gitRepo/mtg/server.js &
+exit 0
+
+
+
+
+cpt=`pgrep node | wc -l`
+if [ "$cpt" -lt 1 ]; then
     nohup /usr/local/bin/node /home/pi/gitRepo/mtg/server.js &
     exit 0
-#else
-#    exit 0
-#fi
-#exit 0
+else
+    exit 0
+fi
+exit 0
