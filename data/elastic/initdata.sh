16802c60ceed1f19a0352256b3a7a003095fb0a6
JulienA <ju.aubert87@gmail.com>
2014-09-20 19:30:54 +0200

    script données elastic
    Signed-off-by: JulienA <ju.aubert87@gmail.com>

--- data/elastic/initdata.sh
+++ data/elastic/initdata.sh
@@ -1,4 +1,4 @@
-curl -XDELETE 'http://192.168.157.130:9999/mtgcard'
-curl -XPUT 'http://192.168.157.130:9999/mtgcard' -d @index_settings.json
-curl -XPUT 'http://192.168.157.130:9999/mtgcard/mtgcard/_mapping' -d @index_mapping_mtg.json
-curl -XPOST 'http://192.168.157.130:9999/_bulk' --data-binary @mtg.json
+curl -XDELETE 'http://192.168.0.22:9200/mtgcard'
+curl -XPUT 'http://192.168.0.22:9200/mtgcard' -d @index_settings.json
+curl -XPUT 'http://192.168.0.22:9200/mtgcard/mtgcard/_mapping' -d @index_mapping_mtg.json
+curl -XPOST 'http://192.168.0.22:9200/_bulk' --data-binary @mtg.json
