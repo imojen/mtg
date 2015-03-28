38fd68005c4f684d12851786001a0ed375538f3a
JulienA <ju.aubert87@gmail.com>
2015-03-28 08:50:30 +0100

    revert nodemon
    Signed-off-by: JulienA <ju.aubert87@gmail.com>

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #!/usr/local/bin/nodemon
-cpt=`pgrep nodemon | wc -l`
+cpt=`pgrep node | wc -l`
 if [ "$cpt" -lt 1 ]; then
-    nohup /usr/local/bin/nodemon /home/pi/gitRepo/mtg/server.js &
+    nohup /usr/local/bin/node /home/pi/gitRepo/mtg/server.js &
     exit 0
 else
     exit 0
