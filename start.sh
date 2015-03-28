3d9007b67364c7deb1db51a2bc37b16df97f727c
JulienA <ju.aubert87@gmail.com>
2015-03-28 08:42:04 +0100

    nodemon try
    Signed-off-by: JulienA <ju.aubert87@gmail.com>

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#!/usr/local/bin/node
-cpt=`pgrep node | wc -l`
+#!/usr/local/bin/nodemon
+cpt=`pgrep nodemon | wc -l`
 if [ "$cpt" -lt 1 ]; then
-    nohup /usr/local/bin/node /home/pi/gitRepo/mtg/server.js &
+    nohup /usr/local/bin/nodemon /home/pi/gitRepo/mtg/server.js &
     exit 0
 else
     exit 0
